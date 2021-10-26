ebf1d72c8445b38efa33f04c0df5c34cec00d110
Victor Weinert <dev@vweinert.com>
2021-10-26 14:44:29 -0500

    added questions, need to write readme

--- index.js
+++ index.js
@@ -1,13 +1,123 @@
-// TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title? (required)',
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log("\nPlease enter a project title");
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Enter project description (required)',
+    validate: descriptionInput => {
+        if (descriptionInput) {
+            return true;
+        } else {
+            console.log('\nPlease enter a description');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions (required)',
+    validate: installationInput => {
+        if (installationInput) {
+            return true;
+        } else {
+            console.log('\nPlease enter installation instructions');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage instructions (required)',
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('\nPlease enter usage instructions');
+            return false;
+        }
+    }
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Select a license (required)',
+    choices: ['No License (Public Domain)', 'GPL', 'BSD', 'Apache', 'MIT'],
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter usage contributing instructions (leave blank for default for Contributor Covenant Code of Conduct',
+    default: ''
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Enter tests (required)',
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('\nPlease enter tests');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter email (required)',
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('\nPlease enter an email address');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'Enter github username (required)',
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('\nPlease enter a github user');
+            return false;
+        }
+    }
+}];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName,data);
+ }
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    inquirer.prompt(questions).then((answers) => {
+        if (answers.contributing === '') {
+            answers.contributing = fs.readFileSync("./utils/coc.md", 'utf-8');
+        }
+        let md = generateMarkdown(answers);
+        writeToFile('readme.md',md);
+    });
+}
 
-// Function call to initialize app
 init();
